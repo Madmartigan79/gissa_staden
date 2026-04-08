2eb1f97a1ce23fc44038436dfdaf3ff29e8e1111
Madmartigan79 <mattias.lagerback@gmail.com>
2026-04-08 22:16:50 +0200

    Add type annotation to handleSuggestionClick parameter

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
     }
   };
 
-  const handleSuggestionClick = (cityName) => {
+  const handleSuggestionClick = (cityName: any) => {
     setGuess(cityName);
     setShowSuggestions(false);
   };
