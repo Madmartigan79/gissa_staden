8b8ed0edca30e358bbcf266bdb8af30882d31e20
Madmartigan79 <mattias.lagerback@gmail.com>
2026-04-09 09:29:56 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // Varning: Detta stänger av kodpolisen helt under bygget. 
-    // Perfekt för att få ut sidan live utan krångel!
-    ignoreDuringBuilds: true,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
